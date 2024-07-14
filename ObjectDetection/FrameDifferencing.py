a1e88b2ec066c6547c2b9c7eeaeb317ce4796de1
201811017 <151823095+201811017@users.noreply.github.com>
2024-07-14 14:00:02 +0200

    Update FrameDifferencing.py

--- ObjectDetection/FrameDifferencing.py
+++ ObjectDetection/FrameDifferencing.py
@@ -8,14 +8,6 @@ import cv2
 from functions import getBGSubtractor, calculate_time
 
 def get_mask(frame1, frame2, kernel=np.array((9,9), dtype=np.uint8)):
-    """ Obtains image mask
-        Inputs: 
-            frame1 - Grayscale frame at time t
-            frame2 - Grayscale frame at time t + 1
-            kernel - (NxN) array for Morphological Operations
-        Outputs: 
-            mask - Thresholded mask for moving pixels
-        """
     frame_diff = cv2.subtract(frame2, frame1)
 
     # blur the frame difference
@@ -32,14 +24,6 @@ def get_mask(frame1, frame2, kernel=np.array((9,9), dtype=np.uint8)):
     return mask
 
 def get_contour_detections(mask, thresh=400):
-    """ Obtains initial proposed detections from contours discoverd on the mask. 
-        Scores are taken as the bbox area, larger is higher.
-        Inputs:
-            mask - thresholded image mask
-            thresh - threshold for contour size
-        Outputs:
-            detectons - array of proposed detection bounding boxes and scores [[x1,y1,x2,y2,s]]
-        """
     # get mask contours
     contours, _ = cv2.findContours(mask, 
                                    cv2.RETR_EXTERNAL, # cv2.RETR_TREE, 
@@ -54,10 +38,6 @@ def get_contour_detections(mask, thresh=400):
     return np.array(detections)
 
 def compute_iou(box1, box2):
-    """ Obtains Intersection over union (IOU) of 2 bounding boxes
-        Inputs are in the form of:
-            xmin, ymin, xmax, ymax = box
-        """
     x11, y11, x21, y21 = box1
     x12, y12, x22, y22 = box2
 
@@ -81,11 +61,6 @@ def compute_iou(box1, box2):
 
 
 def get_inter_area(box1, box2):
-    """
-    Obtains bounding box for intersection area of two boundning boxes
-    Inputs are in the form of:
-            xmin, ymin, xmax, ymax = box
-    """
     x11, y11, x21, y21 = box1
     x12, y12, x22, y22 = box2
 
@@ -103,15 +78,6 @@ def get_inter_area(box1, box2):
     return xi1, yi1, xi2, yi2
 
 def remove_contained_bboxes(boxes):
-    """ Removes all smaller boxes that are contained within larger boxes.
-        Requires bboxes to be soirted by area (score)
-        Inputs:
-            boxes - array bounding boxes sorted (descending) by area 
-                    [[x1,y1,x2,y2]]
-        Outputs:
-            keep - indexes of bounding boxes that are not entirely contained 
-                   in another box
-        """
     check_array = np.array([True, True, False, False])
     keep = list(range(0, len(boxes)))
     for i in keep: # range(0, len(bboxes)):
@@ -126,15 +92,6 @@ def remove_contained_bboxes(boxes):
 
 
 def non_max_suppression(boxes, scores, threshold=1e-1):
-    """
-    Perform non-max suppression on a set of bounding boxes and corresponding scores.
-    Inputs:
-        boxes: a list of bounding boxes in the format [xmin, ymin, xmax, ymax]
-        scores: a list of corresponding scores 
-        threshold: the IoU (intersection-over-union) threshold for merging bounding boxes
-    Outputs:
-        boxes - non-max suppressed boxes
-    """
     # Sort the boxes by score in descending order
     boxes = boxes[np.argsort(scores)[::-1]]
 
@@ -160,17 +117,6 @@ def non_max_suppression(boxes, scores, threshold=1e-1):
     return boxes[keep]
 
 def get_detections(frame1, frame2, bbox_thresh=400, nms_thresh=1e-3, mask_kernel=np.array((15,15), dtype=np.uint8)):
-    """ Main function to get detections via Frame Differencing
-        Inputs:
-            frame1 - Grayscale frame at time t
-            frame2 - Grayscale frame at time t + 1
-            bbox_thresh - Minimum threshold area for declaring a bounding box 
-            nms_thresh - IOU threshold for computing Non-Maximal Supression
-            mask_kernel - kernel for morphological operations on motion mask
-        Outputs:
-            detections - list with bounding box locations of all detections
-                bounding boxes are in the form of: (xmin, ymin, xmax, ymax)
-        """
     # get image mask for moving pixels
     mask = get_mask(frame1, frame2, mask_kernel)
 
@@ -189,13 +135,9 @@ def draw_bboxes(frame, detections):
         x1,y1,x2,y2 = det
         cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 3)
     
-# Change directory
-#os.chdir(r"D:/Videos")
 
-# Create a VideoCapture object and read from input file => CPU
-cap = cv2.VideoCapture("C:\CB041446.mp4")
-
-#cap = cv2.VideoCapture("D:/Videos/AugustoVideoH264.mp4")
+# Create a VideoCapture object and read from input file 
+cap = cv2.VideoCapture("C:\CB041446.mp4") #fill with appropiate full path
 
 # Start the timer
 start_time = cv2.getTickCount()
